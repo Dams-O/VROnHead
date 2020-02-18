ca681830497a9c5827fdc977cd166a4d30657f10
Vathoz05 <val050313@gmail.com>
2020-02-18 16:12:36 +0100

    Script Lancer part 1

---
+++ Assets/script/lancer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using EasyInputVR.Core;
+using System;
+
+public class lancer : MonoBehaviour
+{
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
