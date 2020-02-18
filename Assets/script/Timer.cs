2f1f87ca9f6c3d3eaa48d094604dbe9073bd9fb0
Dams <damien.rabellino@ynov.com>
2020-02-18 15:11:24 +0100

    Ajout Timer et Panier

---
+++ Assets/script/Timer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Slider timer;
+    public float timeLeft = 20.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+     
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        timer.value = timeLeft / 20.0f;
+        if(timeLeft < 0)
+        {
+            Debug.Log("fin timer");
+        }
+    }
+}
