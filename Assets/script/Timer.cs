cfa5b6acec4cacdca7bd224d2b95bf6738b6fbb0
Vathoz05 <val050313@gmail.com>
2020-02-27 08:54:21 +0100

    Son + Timer

--- Assets/script/Timer.cs
+++ Assets/script/Timer.cs
@@ -7,10 +7,16 @@ public class Timer : MonoBehaviour
 {
     public Slider timer;
     public float timeLeft = 20.0f;
+    public int score;
+    public AudioSource audioDefaite;
+    public AudioSource audioVictoire;
+    public AudioSource audioTictac;
+    public AudioSource fondSonore;
     // Start is called before the first frame update
     void Start()
     {
-        
+        fondSonore.Stop();
+        audioTictac.Play();
     }
      
     void Update()
@@ -19,7 +25,18 @@ public class Timer : MonoBehaviour
         timer.value = timeLeft / 20.0f;
         if(timeLeft < 0)
         {
+            if (score >= 20)
+            {
+                audioVictoire.Play();
+            }
+            else
+            {
+                audioDefaite.Play();
+            }
+            audioTictac.Pause();
+            fondSonore.Play();
             Debug.Log("fin timer");
+            Destroy(this);
         }
     }
 }
